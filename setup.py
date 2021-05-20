bd754bb29ac9daac2c797898935df5abc2407961
M4cs <mabridgland@protonmail.com>
2021-05-20 14:22:00 -0400

    Fix Verison

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cluwards',
-    version='1.1.0',
+    version='1.1.1',
     author='Max Bridgland',
     author_email='mabridgland@protonmail.com',
     description='CLUCoin Reward Tracker',
